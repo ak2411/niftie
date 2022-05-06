17d0fe7cf755ff6475ec83e23499a4e134b15a2d
Yiran Wang <yiranw@dhcp-vl2051-1718.eduroam.cornell.edu>
2022-05-06 14:51:35 -0400

    added creatdaobuilder form

--- src/CreateDaoBuilder.js
+++ src/CreateDaoBuilder.js
@@ -1,4 +1,4 @@
-import {Image, Button, Container, ProgressBar, Card} from 'react-bootstrap';
+import {Image, InputGroup, FormControl, Button, Container, ProgressBar, Card} from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import hero from './images/game-bg.png';
 import './css/GameDashboard.css';
@@ -22,14 +22,64 @@ function CreateDaoBuilder() {
     const [createdDAO, setCreatedDAO] = useState(false);
 
     return (
-        <div className="game-dashboard">
-            {createdDAO ? (<Success />) : 
-            (
-            <Card>
-                <Card.Footer><Button variant="primary" size="lg" className="custom-button" onClick={()=> setCreatedDAO(true)}>
-               Create DAO
-            </Button></Card.Footer>
-            </Card>)}
+        <div className="game-dashboard"  style={{display:"flex", gap:"10px"}}>
+            {/* pageProp.hasMetamask && active */}
+            {(true) ? (createdDAO) ? (<Success />) : (
+                <div><h2>Create DAO Builder</h2>
+                <InputGroup>
+                    <InputGroup.Radio aria-label="Radio button for following text input" />
+                    Allow gamers to import existing Discord channel. </InputGroup>
+                <Card style={{padding: "10px"}}>
+                    <Card.Title>Basic information</Card.Title>
+                    <Card.Body>
+                    <InputGroup>
+                    <InputGroup.Text id="basic-addon1">Game Name: </InputGroup.Text>
+                    <FormControl
+                placeholder="name"
+                aria-label="name"
+                aria-describedby="name"
+            /></InputGroup>
+                    <InputGroup>
+                    <InputGroup.Text id="basic-addon1">Token Name: </InputGroup.Text>
+                    <FormControl
+                placeholder="Size"
+                aria-label="Size"
+                aria-describedby="size"
+            /></InputGroup>
+                <InputGroup><InputGroup.Text id="basic-addon1">Color Scheme </InputGroup.Text>
+                        <FormControl
+                    placeholder="admin"
+                    aria-label="admin"
+                    aria-describedby="admin"
+                /></InputGroup>
+                </Card.Body>
+
+                </Card>
+                <Card  style={{padding: "10px"}}>
+                    <Card.Title>Entry Requirements</Card.Title>
+                    <Card.Body>
+                    <InputGroup>
+                    <InputGroup.Checkbox aria-label="Radio button for following text input" />
+                    Require new members join your DAO after pass the voting%? </InputGroup>
+                    <InputGroup>
+                    <InputGroup.Checkbox aria-label="Radio button for following text input" />
+                    Allow admins to admit new members directly?</InputGroup>
+                    </Card.Body>
+                </Card>
+            </div>
+            ):("Please Connect a MetaMask Wallet!")}
+            <div className="game-dashboard">
+            {createdDAO ? (!<Success />) : 
+                <div style={{display:"flex", gap:"10px"}}>
+                <Button variant="primary" size="lg" className="custom-button" style={{backgroundColor:"grey"}} onClick={()=> setCreatedDAO(true)}>
+                    Create DAO
+                </Button>
+                <Button variant="primary" size="lg" className="custom-button" style={{backgroundColor:"grey"}}>
+                    Cancel
+                </Button>
+            </div>
+            }
+            </div>
         </div>
     );
   }
